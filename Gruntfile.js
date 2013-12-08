71b6185e5898837191f69c0b4f4ad9c9945e873e
Joao Figueiredo <Joao.Figueiredo@blip.pt>
2013-12-08 16:51:26 +0000

    Adds build control grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -407,33 +407,27 @@ module.exports = function (grunt) {
             all: {
                 rjsConfig: '<%= yeoman.app %>/scripts/main.js'
             }
+        },
+        buildcontrol: {
+            options: {
+                dir: 'dist',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            pages: {
+                options: {
+                    remote: 'git@github.com:lucalanca/yo-mobile.git',
+                    branch: 'gh-pages'
+                }
+            },
+            local: {
+                options: {
+                    remote: '../',
+                    branch: 'build'
+                }
+            }
         }
-        // buildcontrol: {
-        //     options: {
-        //         dir: 'dist',
-        //         commit: true,
-        //         push: true,
-        //         message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-        //     },
-        //     pages: {
-        //         options: {
-        //             remote: 'git@github.com:example_user/example_webapp.git',
-        //             branch: 'gh-pages'
-        //         }
-        //     },
-        //     heroku: {
-        //         options: {
-        //             remote: 'git@heroku.com:example-heroku-webapp-1988.git',
-        //             branch: 'master'
-        //         }
-        //     },
-        //     local: {
-        //         options: {
-        //             remote: '../',
-        //             branch: 'build'
-        //         }
-        //     }
-        // }
     });
 
     grunt.registerTask('server', function (target) {
